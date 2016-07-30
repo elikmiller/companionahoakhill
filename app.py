ed47fd014db08dfd899afcab16363c4bd7483572
Eli K Miller <eli@elikmiller.com>
2016-07-30 12:38:04 -0500

    blocking complete

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-	return render_template('home.html')
+def about_us():
+	return render_template('about_us.html')
 
 @app.route('/our_services')
 def our_services():
@@ -17,6 +17,10 @@ def our_staff():
 @app.route('/contact_us')
 def contact_us():
 	return render_template('contact_us.html')
+
+@app.route('/base')
+def base():
+	return render_template('base.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
