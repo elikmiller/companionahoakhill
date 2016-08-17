09f2553fa44934912792386be4f6699628e367dd
Eli K Miller <eli@elikmiller.com>
2016-08-17 15:25:07 -0500

    yep

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def about_us():
-	return render_template('about_us.html')
+def home():
+	return render_template('home.html')
 
 @app.route('/our_services')
 def our_services():
